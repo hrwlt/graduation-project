a5e7d4764d3f3cb6008963e26e93b517c8ebfb56
rui.huang <rui.huang@husor.com>
2018-02-27 17:15:57 +0800

    注册表单

--- CodeIgniter/application/views/sign/index.php
+++ CodeIgniter/application/views/sign/index.php
@@ -10,7 +10,8 @@
 <body>
 <h1>注册</h1>
 <form action="sign">
-    
+    <p>用户名</p>
+    <p>密码</p>
 </form>
 <script type="text/javascript" src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
 <script type="text/javascript" src="<?= base_url() . 'resource/bootstrap/js/bootstrap.min.js' ?>"></script>
